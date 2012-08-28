2cb299433d58e80e0bad4093a9adfade5bcc9b3c
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-08-28 12:48:39 -0300

    Added the backbone javascript in template

---
+++ partials/_backbone.rb
@@ -0,0 +1,8 @@
+run "mkdir app/assets/javascripts/lib"
+run "mkdir app/assets/javascripts/app"
+run "echo '' > app/assets/javascripts/app/.gitkeep"
+run "curl http://documentcloud.github.com/underscore/underscore.js > app/assets/javascripts/lib/underscore.js"
+run "curl http://backbonejs.org/backbone.js > app/assets/javascripts/lib/backbone.js"
+
+git :add => '.'
+git :commit => "-qm 'Add backbone.js and underscore.js'"
