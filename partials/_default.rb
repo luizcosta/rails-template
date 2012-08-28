cfb2c39c2f0326b76b744bcdb6ae6182ad488d37
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-08-28 12:49:11 -0300

    changes for default temlate

--- partials/_default.rb
+++ partials/_default.rb
@@ -1,29 +1,13 @@
 puts "Adding default files ...".magenta
 
-copy_static_file 'app/assets/stylesheets/reset.css'
-copy_static_file 'app/views/layouts/application.html.haml'
-copy_static_file 'Procfile'
+copy_static_file 'app/views/layouts/application.html.slim'
+gsub_file 'app/views/layouts/application.html.slim', /PROJECT/, @app_name
+git :add => '.'
+git :commit => "-aqm 'Add application layout in slim.'"
 
-%w{requires.rb}.each do |component|
-  copy_static_file "config/initializers/#{component}"
-end
-
-%w{debug_helper.rb}.each do |component|
-  copy_static_file "lib/#{component}"
-end
-
-%w{debug_helper_spec.rb}.each do |component|
-  copy_static_file "spec/lib/#{component}"
-end
-
-%w{integration.rake deploy.rake}.each do |component|
-  copy_static_file "lib/tasks/#{component}"
-end
-
-copy_static_file 'config/locales/pt-BR.yml'
-
-gsub_file 'lib/tasks/deploy.rake', /PROJECT/, @app_name
-gsub_file 'lib/tasks/integration.rake', /PROJECT/, @app_name
+copy_static_file 'config/locales/rails.pt-BR.yml'
+git :add => '.'
+git :commit => "-aqm 'Add rails locale for pt-BR.'"
 
 copy_static_file '.gitignore'
 
