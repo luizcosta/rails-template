296f8252802d08e7e67ab9b198ca9e1fb1020ab2
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-08-28 08:58:38 -0300

    changed the database.yml to database.yml.sample

--- partials/_database.rb
+++ partials/_database.rb
@@ -1,6 +1,6 @@
-copy_static_file 'config/database.yml'
-gsub_file 'config/database.yml', /PROJECT/, @app_name
-git :add => 'config/database.yml'
-git :commit => "-qm 'Adding config/database.yml.'"
+copy_static_file 'config/database.yml.sample'
+gsub_file 'config/database.yml.sample', /PROJECT/, @app_name
+git :add => 'config/database.yml.sample'
+git :commit => "-qm 'Adding config/database.yml.sample.'"
 
 puts "\n"
