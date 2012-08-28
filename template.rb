43a6fecedfbf81bf2444e89756700a9e0d2ecca3
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-08-28 13:18:47 -0300

    added heroku template again

--- template.rb
+++ template.rb
@@ -62,11 +62,7 @@ apply_n :javascripts
 apply_n :generators
 apply_n :gems
 apply_n :rvm
-#apply_n :heroku
-
-# apply_n :omniauth # TODO: add spec support files
-                    # TODO: take care of facebook when user is not logged in on facebook (when app)
-# TODO: extrair phone validator to gem
+apply_n :heroku
 
 run 'bundle install'
 puts "\nRunning after Bundler callbacks."
