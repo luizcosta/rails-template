4a830ddaaa546b0f68f5d6c7bd651b26193d8957
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-08-28 18:35:38 -0300

    solved problem

--- partials/_stylesheets.rb
+++ partials/_stylesheets.rb
@@ -1,6 +1,6 @@
 say "Configuring stylesheets...".magenta
 
-run 'mv app/assets/stylesheets/application.css app/assets/stylesheets/application.sassmv app/assets/stylesheets/application.css app/assets/stylesheets/application.sass'
+run 'mv app/assets/stylesheets/application.css app/assets/stylesheets/application.sass'
 
 git :add => '.'
 git :rm => 'app/assets/stylesheets/application.css'
@@ -9,7 +9,8 @@ git :commit => "-aqm 'Renamed application.css to application.sass'"
 if would_you_like? "Use twitter bootstrap? [y,n]".red
   gsub_file 'Gemfile', /#gem 'bootstrap-sass/, "gem 'bootstrap-sass"
 end
-run "echo '\n@import bootstrap' > app/assets/stylesheets/application.sass"
+run "echo '\n@import bootstrap' >> app/assets/stylesheets/application.sass"
 
 git :add => '.'
 git :commit => "-aqm 'Add twitter bootstrap'"
+
