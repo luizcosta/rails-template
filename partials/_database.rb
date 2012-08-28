cb28476c7787181b95d72889f73f92f3ca02f319
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-08-28 17:11:58 -0300

    replacing the database.yml

--- partials/_database.rb
+++ partials/_database.rb
@@ -1,6 +1,7 @@
 copy_static_file 'config/database.sample.yml'
 gsub_file 'config/database.sample.yml', /PROJECT/, @app_name
 git :add => 'config/database.sample.yml'
+run 'cp -f config/database.sample.yml config/database.yml'
 git :commit => "-qm 'Adding config/database.sample.yml.'"
 
 puts "\n"
