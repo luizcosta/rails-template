357860fbd66180fd0c5fe1bb6bbe7b3c247c3abc
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-08-28 18:29:28 -0300

    Created the stylesheets template

---
+++ partials/_stylesheets.rb
@@ -0,0 +1,15 @@
+say "Configuring stylesheets...".magenta
+
+run 'mv app/assets/stylesheets/application.css app/assets/stylesheets/application.sassmv app/assets/stylesheets/application.css app/assets/stylesheets/application.sass'
+
+git :add => '.'
+git :rm => 'app/assets/stylesheets/application.css'
+git :commit => "-aqm 'Renamed application.css to application.sass'"
+
+if would_you_like? "Use twitter bootstrap? [y,n]".red
+  gsub_file 'Gemfile', /#gem 'bootstrap-sass/, "gem 'bootstrap-sass"
+end
+run "echo '\n@import bootstrap' > app/assets/stylesheets/application.sass"
+
+git :add => '.'
+git :commit => "-aqm 'Add twitter bootstrap'"
